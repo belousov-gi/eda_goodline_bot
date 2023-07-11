b0ecb5f899329cb9f675c272777f00fad08336c2
belousov-gi <belousov.gi@yandex.ru>
2023-07-11 22:50:12 +0700

    add

---
+++ eda_goodline_bot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace eda_goodline_bot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
